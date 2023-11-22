eed769628d21867deff672d02b70c9cec2e8b7e7
dsalgadob <dsalgado@zircon.tech>
2023-11-22 15:39:42 -0500

    feat: add api call

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
 'use client'; // If used in Pages Router, is no need to add "use client"
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'antd';
 
-const Home = () => (
+const Home = () => {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1/api/", {
+      method: "GET"
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data[0].joke);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
   <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+    <Button type="primary">{message}</Button>
+  </div>);
+}
+;
 
 export default Home;
